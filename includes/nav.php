ffe63fa2deb01ce62c87bf35e66c0e0b4644de92
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-06-25 12:37:27 +0200

    made a simple map structure and started working on the nav

---
+++ includes/nav.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Matthijs Berghuis</title>
+</head>
+<body>
+<nav>
+    <div class="nav_links">
+        <a href="http://"> < MATTHIJS /> </a>
+    </div>    
+
+    <div class="nav_rechts">
+        <a href="http://">HOME</a>
+        <a href="http://">CONTACT</a>
+        <a href="http://">NOTHING</a>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include 'includes/nav.php'; ?>
+
+<?php include 'includes/footer.php'; ?>
