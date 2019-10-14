1e2422760eb51be17b2954fa9aa45d9873534ae3
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-10-14 14:03:41 +0200

    started on footer.php

--- private/includes/footer.php
+++ private/includes/footer.php
@@ -1,5 +1,10 @@
 <footer>
-    
+
 </footer>
+<script type="text/javascript" src="public/assets/js/modaal.js"></script>
+<script type="text/javascript" src="public/assets/js/contact.js"></script>
+<script type="text/javascript" src="public/assets/js/nav.js"></script>
+<script type="text/javascript" src="public/assets/js/slider.js"></script>
 </body>
+
 </html>
