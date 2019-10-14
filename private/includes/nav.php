6e5591e98186764442966c4689c79f24693b639b
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-10-14 14:03:05 +0200

    created my nav.php (still working on it)

--- private/includes/nav.php
+++ private/includes/nav.php
@@ -1,20 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="My name is Matthijs Berghuis, I am a 17-year-old full-stack webdeveloper. This is the portfolio i've created. Come take a look!">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript,PHP,Full-stack,Mediacollege,Assendelft,Student,Developer,Dev,Web-dev">
+    <meta name="author"s content="Matthijs">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"></head>
-<body>
-<nav>
-    <div class="nav_links">
-        <a href="/"> < MATTHIJS > </a>
-    </div>    
+    <link rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="icon" href="public/assets/img/other/_m.png" type="image/png" sizes="16x16">
+    <title>Matthijs Berghuis Portfolio</title>
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+</head>
 
-    <div class="nav_rechts">
-        <a href="/">ABOUT</a>
-        <a href="/">PORTFOLIO</a>
-        <a href="/">CONTACT</a>
-    </div>
-</nav>
+<body>
+    <nav>
+        <div class="nav_links template__nav-item">
+            <svg id="Laag_1" data-name="Laag 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1899.35 996.25">
+                <defs>
+                </defs>
+                <title>&amp;lt;M&amp;gt;</title>
+                <polygon class="cls-1" points="1898.85 587.62 1390.18 182.83 1390.18 1.04 1898.85 405.83 1898.85 587.62" />
+                <polygon class="cls-2" points="1898.85 408.25 1390.18 813.04 1390.18 994.83 1898.85 590.04 1898.85 408.25" />
+                <polygon class="cls-1" points="0.5 587.62 509.17 182.83 509.17 1.04 0.5 405.83 0.5 587.62" />
+                <polygon class="cls-2" points="0.5 408.25 509.17 813.04 509.17 994.83 0.5 590.04 0.5 408.25" />
+                <path class="cls-2" d="M638.82,1037.73V43.46H775.67L958.6,569.92h2.79L1142.93,43.46h138.25v994.27H1138.74V433.07H1136l-141,427.31H923.69L784.05,433.07h-2.8v604.66Z" transform="translate(-10.15 -41.98)" />
+            </svg>
+        </div>
+
+        <div class="nav_rechts template__nav-item ">
+        <a class="nav-link" href="#about">ABOUT</a>
+                <a class="nav-link" href="#portfolio">PORTFOLIO</a>
+                <a class="nav-link" href="#contact">CONTACT</a>
+        </div>
+
+        <div class="nav_rechts1">
+            <input type="checkbox" id="nav-toggle" style="display:none;">
+            <label class="nav-toggle-label" for="nav-toggle">
+                <span class="nav-toggle-label-span-1"></span>
+                <span class="nav-toggle-label-span-2"></span>
+                <span class="nav-toggle-label-span-3"></span>
+            </label>
+            <div class="nav-links-container">
+                <a class="nav-link" href="#about">ABOUT</a>
+                <a class="nav-link" href="#portfolio">PORTFOLIO</a>
+                <a class="nav-link" href="#contact">CONTACT</a>
+            </div>
+        </div>
+    </nav>
