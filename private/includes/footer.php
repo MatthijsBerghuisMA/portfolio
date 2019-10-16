d74dcd2482a69358d0329908589440f97d22b923
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-10-16 09:35:14 +0200

    updated the footer

--- private/includes/footer.php
+++ private/includes/footer.php
@@ -5,23 +5,23 @@
                 <span class="smedia_header">social media</span>
                 <div class="header_line"></div>
                 <div class="social_list">
-                    <a href="https://www.instagram.com/matthijs.berghuis/">Instagram</a>
-                    <a href="https://www.facebook.com/matthijs.berghuis.334">Facebook</a>
-                    <a href="https://github.com/matthijsdevelopment">Github</a>
+                    <a href="https://www.instagram.com/matthijs.berghuis/" target="_blank">Instagram</a>
+                    <a href="https://www.facebook.com/matthijs.berghuis.334" target="_blank">Facebook</a>
+                    <a href="https://github.com/matthijsdevelopment" target="_blank">Github</a>
                 </div>
             </div>
             <div class="footer_content">
                 <span class="smedia_header">Contact</span>
                 <div class="header_line"></div>
                 <div class="social_list">
-                    <a href="https://www.linkedin.com/in/matthijs-berghuis-9447b617a/">LinkedIn</a>
-                    <a href="mailto:matthijsdevelopment@gmail.com">Email</a>
-                    <a href="tel:+0612235440">0612235440</a>
+                    <a href="https://www.linkedin.com/in/matthijs-berghuis-9447b617a/" target="_blank">LinkedIn</a>
+                    <a href="mailto:matthijsdevelopment@gmail.com" target="_blank">Email</a>
+                    <a href="tel:+0612235440" target="_blank">0612235440</a>
                 </div>
             </div>
         </div>
     </div>
-    <span>Copyright &copy;2019 All rights reserved | MatthijsDevelopment</span>
+    <span class="copyright">Copyright &copy;2019 All rights reserved | MatthijsDevelopment</span>
 </footer>
 <script type="text/javascript" src="public/assets/js/modaal.js"></script>
 <script type="text/javascript" src="public/assets/js/contact.js"></script>
