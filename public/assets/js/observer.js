d5af0ebe79744b158bde817183504c9b1073b482
Matthijs Berghuis <Berghuismatthijs@gmail.com>
2019-11-12 16:43:23 +0100

    updated the Intersection Observer and added a "go to top" button.

--- public/assets/js/observer.js
+++ public/assets/js/observer.js
@@ -1,8 +1,14 @@
 const nav = document.querySelector(".nav");
+const Top = document.querySelector(".top");
 const sectionOne = document.querySelector(".menu_trigger");
-const navObserver = new IntersectionObserver(updateNav, {rootMargin: "-80% 0px 0px 0px"});
+const navObserver = new IntersectionObserver(updateNav, {
+    rootMargin: "-80% 0px 0px 0px"
+});
+const topObserver = new IntersectionObserver(updateTop, {
+    rootMargin: "100px 0px 0px 0px"
+});
 
-function updateNav (entries) {
+function updateNav(entries) {
     entries.forEach((entry) => {
         if (!entry.isIntersecting) {
             console.log("add");
@@ -14,4 +20,17 @@ function updateNav (entries) {
     });
 }
 
+function updateTop(entries) {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+            console.log("add");
+            Top.classList.add("show");
+        } else {
+            console.log('remove');
+            Top.classList.remove("show");
+        }
+    });
+}
+
 navObserver.observe(sectionOne);
+topObserver.observe(sectionOne);
